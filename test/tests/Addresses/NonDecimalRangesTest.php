4fa9eb07c233e517ffbdaf9bbeb7fed1dc66c056
Michele Locati <michele@locati.it>
2020-07-09 12:32:34 +0200

    Add support for non-decimal IP/range addresses

---
+++ test/tests/Addresses/NonDecimalRangesTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace IPLib\Test\Addresses;
+
+use IPLib\Factory;
+use IPLib\Test\TestCase;
+
+class NonDecimalRangesTest extends TestCase
+{
+    public function casesProvider()
+    {
+        return array(
+            array('07.0.0.*', false, '7.0.0.*'),
+            array('07.0.0.*', true, '7.0.0.*'),
+            array('08.0.0.*', false, '8.0.0.*'),
+            array('08.0.0.*', true),
+            array('010.0.0.*', false, '10.0.0.*'),
+            array('010.0.0.*', true, '8.0.0.*'),
+            array('00000000000000000000000000000000000000010.0.0.*', false, '10.0.0.*'),
+            array('00000000000000000000000000000000000000010.0.0.*', true, '8.0.0.*'),
+            array('07.0.0.123/32', false, '7.0.0.123/32'),
+            array('07.0.0.123/32', true, '7.0.0.123/32'),
+            array('08.0.0.123/32', false, '8.0.0.123/32'),
+            array('08.0.0.123/32', true),
+            array('010.0.0.123/32', false, '10.0.0.123/32'),
+            array('010.0.0.123/32', true, '8.0.0.123/32'),
+            array('0x10.0.0.123/32', false),
+            array('0x10.0.0.123/32', true, '16.0.0.123/32'),
+            array('000000000000000000000000000000000000000x10.0.0.123/32', true, '16.0.0.123/32'),
+            array('0x00000000000000000000000000000000000000010.0.0.123/32', true, '16.0.0.123/32'),
+            array('000000000000000000000000000000000000000x00000000000000000000000000000000000000010.0.0.123/32', true, '16.0.0.123/32'),
+            array('07.0.0.123', false, '7.0.0.123'),
+            array('07.0.0.123', true, '7.0.0.123'),
+            array('08.0.0.123', false, '8.0.0.123'),
+            array('08.0.0.123', true),
+            array('010.0.0.123', false, '10.0.0.123'),
+            array('010.0.0.123', true, '8.0.0.123'),
+            array('0x10.0.0.123', false),
+            array('0x10.0.0.123', true, '16.0.0.123'),
+            array('000000000000000000000000000000000000000x10.0.0.123', true, '16.0.0.123'),
+            array('0x00000000000000000000000000000000000000010.0.0.123', true, '16.0.0.123'),
+            array('000000000000000000000000000000000000000x00000000000000000000000000000000000000010.0.0.123', true, '16.0.0.123'),
+        );
+    }
+
+    /**
+     * @dataProvider casesProvider
+     *
+     * @param string $input
+     * @param string|null $expectedStringRepresentation
+     */
+    public function testCases($input, $parseNonDecimal, $expectedStringRepresentation = null)
+    {
+        $range = Factory::rangeFromString($input, $parseNonDecimal);
+        if (null === $expectedStringRepresentation) {
+            $this->assertNull($range);
+        } else {
+            $this->assertInstanceOf('\IPLib\Range\RangeInterface', $range);
+            $this->assertSame($expectedStringRepresentation, (string) $range->toString());
+        }
+    }
+}
